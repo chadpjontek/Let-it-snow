54967e1d9c547c24ee456d171fac92607572631f
Chad Pjontek <chadpjontek@gmail.com>
2017-12-22 17:00:42 -0500

    added mouse functionality and new images

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+var docHeight = document.body.clientHeight;
 function randomSize() {
   return Math.floor(Math.random() * (91)) + 10;
 }
@@ -6,32 +6,47 @@ function randomSnowflake() {
   return Math.floor(Math.random() * (20)) + 0;
 }
 function fallDistance() {
-  return Math.floor(Math.random() * 1000) + 500;
+  return Math.floor(Math.random() * 1000) + docHeight;
 }
 function rotationSpeed() {
   return Math.floor(Math.random() * (721)) + -360;
 }
 var touchArea = document.getElementById("toucharea");
 
-touchArea.addEventListener("touchstart", function (e) {
-  snow(e);
-});
+touchArea.addEventListener("touchstart", touchSnow);
 
-touchArea.addEventListener("touchmove", function (e) {
+touchArea.addEventListener("touchmove", touchSnow);
+
+touchArea.addEventListener("mousedown", function() {
+  touchArea.addEventListener("mousemove", mouseSnow);
+});         
+
+touchArea.addEventListener("mouseup", endMouseSnow);
+
+function endMouseSnow(e) {
+  e.preventDefault();
+  touchArea.removeEventListener("mousemove", mouseSnow);
+}
+
+function mouseSnow(e){
+  e.preventDefault();
   snow(e);
-});
+}
+
+function touchSnow(e) {
+  e.preventDefault();
+  var touch = e.changedTouches[0];
+  snow(touch);
+}
 
 function snow(e) {
-  e.preventDefault();
-
-  var touch = e.changedTouches[0];
-  var posX = touch.clientX - 25;
-  var posY = touch.clientY - 25;
+  var posX = e.clientX - 25;
+  var posY = e.clientY - 25;
 
   var span = document.createElement("span");
   var size = randomSize();
   var snowflake = randomSnowflake();
-  span.style.background = "url(/svgs/snowflake-" +   + ") no-repeat";
+  span.style.background = "url(svgs/snowflake-" + snowflake + ".svg) no-repeat";
   span.style.height = size + "px";
   span.style.width = size + "px";
   span.style.left = posX + "px";
@@ -41,10 +56,10 @@ function snow(e) {
 
   setTimeout(function(){
     span.style.transform = "rotate(" + rotationSpeed() + "deg)"
-    span.style.top = fallDistance() + posY + 'px';
+    span.style.top = fallDistance() - posY + 'px';
   },0)
  
   setTimeout(function() {
     span.remove();
-  },10000);
+  },20000);
 }
